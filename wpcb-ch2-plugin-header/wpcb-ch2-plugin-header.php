065d5cf4a82f4b6e5020c963068760aca95128f1
Brian Abbott <hi@brianabbott.com>
2025-08-04 22:45:52 -0400

    create plugin header

---
+++ wpcb-ch2-plugin-header/wpcb-ch2-plugin-header.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Chapter 2 - Plugin Header
+ * Plugin URI: https://news.google.com
+ * Description: Declares a plugin that will be visible in the WordPress Admin Dashboard
+ * Version 1.0.0
+ * Author: Brian Abbott
+ * Author URI: https://brianabbott.com
+ * License: GPLv2
+ */
