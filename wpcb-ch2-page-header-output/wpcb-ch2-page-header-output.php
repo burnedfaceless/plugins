edf5544f74855c5e4d03ef94d33170902f61dae0
Brian Abbott <hi@brianabbott.com>
2025-08-30 16:16:50 -0400

    wrote plugin to output google analytics code to website header

---
+++ wpcb-ch2-page-header-output/wpcb-ch2-page-header-output.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Plugin Name: Chapter 2 - Page Header Output
+ * Plugin URI: https://news.google.com
+ * Description: Outputs Google Analytics Code to the page header
+ * Version 1.0.0
+ * Author: Brian Abbott
+ * Author URI: https://brianabbott.com
+ * License: GPLv2
+ */
+
+add_action( 'wp_head', 'ch2pho_page_header_output' );
+
+function ch2pho_page_header_output() { ?>
+
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;
+          i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
+          a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;
+          m.parentNode.insertBefore(a,m)})(window,document,'script',
+          'https://www.google-analytics.com/analytics.js','ga');
+
+
+      ga( 'create', 'UA-0000000-0', 'auto' );
+      ga( 'send', 'pageview' );
+  </script>
+
+<?php }
