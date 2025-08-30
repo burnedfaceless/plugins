f347c4b476cbe709eb1e19ce89c39fef9978ad96
Brian Abbott <hi@brianabbott.com>
2025-08-30 17:02:27 -0400

    wrote plugin to display favicon to site header, unless a favicon was set in customizer

---
+++ wpcb-ch2-favicon/index.php
@@ -0,0 +1,3 @@
+<?php
+
+  // prevent dir listing of plugin dir
---
+++ wpcb-ch2-favicon/wpcb-ch2-favicon.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Plugin Name: Chapter 2 - Favicon
+ * Plugin URI: https://news.google.com
+ * Description: Declares a plugin that will be visible in the WordPress Admin Dashboard
+ * Version 1.0.0
+ * Author: Brian Abbott
+ * Author URI: https://brianabbott.com
+ * License: GPLv2
+ */
+
+function ch2fi_page_header_output(): void {
+  $site_icon_url = get_site_icon_url();
+  if ( !empty( $site_icon_url ) ) {
+    wp_site_icon();
+  } else {
+    $icon = plugins_url( '/res/favicon.ico', __FILE__  );
+    ?>
+
+  <link rel="shortcut icon" href="<?php echo esc_url( $icon ); ?>"  />
+ <?php }
+}
+
+add_action( 'wp_head', 'ch2fi_page_header_output' );
