8fc495d35004011578ea554663921e11c5787f99
Brian Abbott <hi@brianabbott.com>
2025-09-02 05:49:18 -0400

    finished plugin

--- wpcb-ch2-nav-menu-filter/wpcb-ch2-nav-menu-filter.php
+++ wpcb-ch2-nav-menu-filter/wpcb-ch2-nav-menu-filter.php
@@ -11,7 +11,19 @@
  */
 
 function ch2_new_nav_menu_items( $sorted_menu_items, $args ) {
-  print_r( $sorted_menu_items );
+  // check to see if user is logged in, continue if not logged in
+  if ( !is_user_logged_in() ) {
+    // loop through all menu items received
+    // place each item's key in $key variable
+    foreach ( $sorted_menu_items as $key => $sorted_menu_item  ) {
+      // Check if menu item matches search string
+      if ( 'Private Area' == $sorted_menu_item->title ) {
+        // remove item from menu array if found
+        // using item key
+        unset( $sorted_menu_items[ $key ] );
+      }
+    }
+  }
   return $sorted_menu_items;
 }
 
